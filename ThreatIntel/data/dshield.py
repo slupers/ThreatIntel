6f1e8a3d910f55260c4c72168d13e6040493351f
Andrew Dunn <adunn3@gmail.com>
2013-04-20 15:32:35 -0400

    dshield modified

--- ThreatIntel/data/dshield.py
+++ ThreatIntel/data/dshield.py
@@ -1,13 +1,13 @@
 import urllib2
 
-urlbase =  "http://www.dshield.org/api/topips/records/10/"
+class DShield():
+    def __init__(self):
+        self= "test"
 
-datelist=["2013-04-16","2013-04-15","2013-04-14","2013-04-13","2013-04-12","2013-04-11"]
-for element in datelist:
-    ipdata=urllib2.urlopen(urlbase+element)
-    fp=open(element+".xml","w")
-    fp.write(ipdata.read())
-    fp.close()
+    def lookup_ip(ip):
+	urlbase = "http://www.dshield.org/api/ip/"
+        return urllib2.urlopen(urlbase+ip).read()
 
 
+    print(lookup_ip("4.2.2.1"))
 
