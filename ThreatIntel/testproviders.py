f7dc25583c994befecfd50de5796399c69a9320c
Andrew Mintner <mintner1@illinois.edu>
2013-04-20 16:20:55 -0500

    Cleaned up test code

--- ThreatIntel/testproviders.py
+++ ThreatIntel/testproviders.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 from data import *
 
-def printresult(res):
+providers = [
+    PhishTankDataProvider(),
+    DShieldDataProvider()
+]
+queries = [
+    ("http://onbe.ru/lISy", DataProvider.URL_QUERY),
+    ("4.2.2.1", DataProvider.IPV4_QUERY)
+]
+
+for target, qtype in queries:
+    res = DataProvider.queryn(target, qtype, providers)
     for p, iset in res:
         print "Provider: {0}".format(p.name)
         print "Disposition: {0}".format(iset.disposition)
         for k, v in iset.facets:
             print "{0}: {1}".format(k, v)
-
-pdp = PhishTankDataProvider()
-ddp = DShieldDataProvider()
-printresult(DataProvider.queryn("http://onbe.ru/lISy", DataProvider.URL_QUERY, [ddp, pdp]))
-printresult(DataProvider.queryn("4.2.2.1", DataProvider.IPV4_QUERY, [ddp, pdp]))
