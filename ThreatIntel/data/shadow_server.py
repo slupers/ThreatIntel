abe6a6b4b8ac177be57c79ce15d71a43bc3db1fc
Andrew Mintner <mintner1@illinois.edu>
2013-04-20 16:37:06 -0500

    Cleaned up shadow_server.py

--- ThreatIntel/data/shadow_server.py
+++ ThreatIntel/data/shadow_server.py
@@ -1,44 +1,42 @@
+import gevent.monkey
 import socket
 import urllib2
+from .base import DataProvider, InformationSet
 
-class shadow_server():
-    
-    global sockfd
-    
-    def __init__(self):
+gevent.monkey.patch_socket()
+
+class ShadowServerDataProvider(DataProvider):
+    @staticmethod
+    def _peerlookup(target):
         self.sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sockfd.connect(("asn.shadowserver.org",43))
-
-    def peer(self, ip):
-        
-        #send and receive the response
         sent = self.sockfd.send("peer {0}\n".format(ip))
         response = self.sockfd.recv(1024)
-            
-        #process the response
-        response_list = response.split(' | ', response.count(' | '))
-
-        response_dict = {}
-        response_dict['peers'] = response_list[0].split(' ',response_list[0].count(' '))
-        response_dict['asn'] = response_list[1]        
-        response_dict['prefix'] = response_list[2]
-        response_dict['as_name'] = response_list[3]
-        response_dict['cn'] = response_list[4]
-        response_dict['domain'] = response_list[5]
-        response_dict['isp'] = response_list[6]
-
-        #return InformationSet(self.name, InformationSet.INFORMATIONAL, **response_dict)
+        cmps = response.split(' | ', response.count(' | '))
+        info = {}
+        info['peer_as_numbers'] = [int(asn) for asn in cmps[0].split(' ')]
+        info['as_number'] = cmps[1]
+        info['network_prefix'] = cmps[2]
+        info['as_name'] = cmps[3]
+        info['country'] = cmps[4]
+        info['domain'] = cmps[5]
+        info['isp'] = cmps[6]
+        return InformationSet(InformationSet.INFORMATIONAL, **info)
     
-    def av(self, md5):
-        response = urllib2.urlopen("http://innocuous.shadowserver.org/api/?query=" + md5 + ":43")
-            
+    @staticmethod
+    def _avlookup(md5):
+        return None
+        #response = urllib2.urlopen("http://innocuous.shadowserver.org/api/?query=" + md5 + ":43")
 
-    def close(self):
-        self.sockfd.close()
+    @property
+    def name(self):
+        return "shadowserver"
 
-if __name__ == '__main__':
-    s = shadow_server()
-    #s.peer("8.8.8.8")
-    s.av("00000142988AFA836117B1B572FAE4713F200567")
-    s.close()
-
+    def query(self, target, qtype):
+        if qtype == DataProvider.IPV4_QUERY:
+            return _peerlookup(target)
+        elif qtype == DataProvider.MD5_QUERY:
+            return _avlookup(target)
+        elif qtype == DataProvider.SHA1_QUERY:
+            return _avlookup(target)
+        return None
