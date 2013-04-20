3c495f46f71ab8851881bb9cc0eac3ba7aee18c2
srgetty <getty3@illinois.edu>
2013-04-20 11:59:00 -0500

    changed to class as per request

--- ThreatIntel/data/shadow_server.py
+++ ThreatIntel/data/shadow_server.py
@@ -2,99 +2,72 @@ import os
 import json as simplejson
 import sys
 
+class shadow_server(object):
 
-def print_file(filename):
-	f = open(filename)
-	s = f.read()
-	print s
-	
-def whitelisted(hashfile):
-    p = os.popen('curl -s http://bin-test.shadowserver.org/api -F filename.1=@%s' % hashfile)
-    data = p.read()
-    p.close()
-    res = {}
-    for line in data.split('\n'):
-        l = line.split(' ', 1)
-        if len(l) == 2:
-            try: res[l[0]] = simplejson.loads(l[1])
-            except: pass
-    return res
+    def print_file(self, filename):
+        f = open(filename)
+        s = f.read()
+        print s
+    
+    def whitelisted(self, hashfile):
+        p = os.popen('curl -s http://bin-test.shadowserver.org/api -F filename.1=@%s' % hashfile)
+        data = p.read()
+        p.close()
+        res = {}
+        for line in data.split('\n'):
+            l = line.split(' ', 1)
+            if len(l) == 2:
+                try: res[l[0]] = simplejson.loads(l[1])
+                except: pass
+        return res
 
-def whois_origin(ip):
-	command = "whois -h asn.shadowserver.org origin " + ip + " > whois_origin.txt"
-	os.system(command)
-	print_file("whois_origin.txt")
+    def whois_origin(self, ip):
+        command = "whois -h asn.shadowserver.org origin " + ip + " > whois_origin.txt"
+        os.system(command)
+        self.print_file("whois_origin.txt")
 
-def whois_peer(ip):
-	command = "whois -h asn.shadowserver.org peer " + ip + " > whois_peer.txt"
-	os.system(command)
-	print_file("whois_peer.txt")
+    def whois_peer(self, ip):
+        command = "whois -h asn.shadowserver.org peer " + ip + " > whois_peer.txt"
+        os.system(command)
+        self.print_file("whois_peer.txt")
 
-def whois_prefix(pre):
-	command = "whois -h asn.shadowserver.org prefix " + pre + " > whois_prefix.txt"
-	os.system(command)
-	print_file("whois_prefix.txt")
-	
-def dns_origin(ip):
-	command = "dig +short " + ip + ".origin.asn.shadowserver.org TXT > dns_origin.txt"
-	os.system(command)
-	print_file("dns_origin.txt")
+    def whois_prefix(self, pre):
+        command = "whois -h asn.shadowserver.org prefix " + pre + " > whois_prefix.txt"
+        os.system(command)
+        self.print_file("whois_prefix.txt")
+    
+    def dns_origin(self, ip):
+        command = "dig +short " + ip + ".origin.asn.shadowserver.org TXT > dns_origin.txt"
+        os.system(command)
+        self.print_file("dns_origin.txt")
 
-def dns_peer(ip):
-	command = "dig +short " + ip + ".peer.asn.shadowserver.org TXT > dns_peer.txt"
-	os.system(command)
-	print_file("dns_peer.txt")
+    def dns_peer(self, ip):
+        command = "dig +short " + ip + ".peer.asn.shadowserver.org TXT > dns_peer.txt"
+        os.system(command)
+        self.print_file("dns_peer.txt")
 
-def av(md5):
-	command = "wget -q -o - http://innocuous.shadowserver.org/api/?query=" + md5 + " > av.txt"
-	os.system(command)
-	print_file("av.txt")
+    def av(self, md5):
+        command = "wget -q -o - http://innocuous.shadowserver.org/api/?query=" + md5 + " > av.txt"
+        os.system(command)
+        self.print_file("av.txt")
 
-def menu():
-	print "1: whitelisted"
-	print "2: whois_origin"
-	print "3: whois_peer"
-	print "4: whois_prefix"
-	print "5: dns_origin"
-	print "6: dns_peer"
-	print "7: av"
-	
-	choice = raw_input()
-	
-	if choice == "1":
-			print "enter a hashfile"
-			hashfile = raw_input()
-			whitelisted(hashfile)
-	if choice == "2":
-			print "enter ip"
-			ip = raw_input()
-			whois_origin(ip)
-	if choice == "3":
-			print "enter ip"
-			ip = raw_input()
-			whois_peer(ip)
-	if choice == "4":
-			print "enter prefix"
-			pre = raw_input()
-			whois_prefix(pre)
-	if choice == "5":
-			print "enter ip"
-			ip = raw_input()
-			dns_origin(ip)
-	if choice == "6":
-			print "enter ip"
-			ip = raw_input()
-			dns_peer(ip)
-	if choice == "7":
-			print "enter md5"
-			md5 = raw_input()
-			av(md5)
-	
+    def execute_ip(self, ip):
+        self.whois_origin(ip)
+        self.whois_peer(ip)
+        self.dns_origin(ip)
+        self.dns_peer(ip)
+
+    def execute_md5(self, md5):
+        self.av(md5)
+
+    def execute_hash(self, hashfile):
+        self.whitelisted(hashfile)
+
+    def __init__(self):
+        print "hello from init"
+        
 
 if __name__ == '__main__':
-	cont = "yes"
-	while(cont == "yes"):
-		menu()
-		print("continue?")
-		cont = raw_input()
-	
+    ss = shadow_server()
+    print "hello"
+    
