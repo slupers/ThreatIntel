73cc73809167520625b2565e5b6daceb70c58c57
Andrew Mintner <mintner1@illinois.edu>
2013-04-20 16:18:05 -0500

    Finished consolidating test code

--- ThreatIntel/testdshield.py
+++
@@ -1,9 +0,0 @@
-from data import *
-
-pdp = DShieldDataProvider()
-res = DataProvider.queryn("4.2.2.1", DataProvider.IPV4_QUERY, [pdp])
-for p, iset in res:
-    print "Provider: {0}".format(p.name)
-    print "Disposition: {0}".format(iset.disposition)
-    for k, v in iset.facets:
-        print "{0}: {1}".format(k, v)
--- ThreatIntel/testproviders.py
+++ ThreatIntel/testproviders.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
 from data import *
 
+def printresult(res):
+    for p, iset in res:
+        print "Provider: {0}".format(p.name)
+        print "Disposition: {0}".format(iset.disposition)
+        for k, v in iset.facets:
+            print "{0}: {1}".format(k, v)
+
 pdp = PhishTankDataProvider()
-res = DataProvider.queryn("http://onbe.ru/lISy", DataProvider.URL_QUERY, [pdp])
-for p, iset in res:
-    print "Provider: {0}".format(p.name)
-    print "Disposition: {0}".format(iset.disposition)
-    for k, v in iset.facets:
-        print "{0}: {1}".format(k, v)
+ddp = DShieldDataProvider()
+printresult(DataProvider.queryn("http://onbe.ru/lISy", DataProvider.URL_QUERY, [ddp, pdp]))
+printresult(DataProvider.queryn("4.2.2.1", DataProvider.IPV4_QUERY, [ddp, pdp]))
