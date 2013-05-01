9288b6ac15e3246a5c0df6194754dceae4214cf9
Andrew Mintner <mintner1@illinois.edu>
2013-05-01 16:12:59 -0500

    Fixed up wsgi.py

--- ThreatIntel/ThreatIntel/wsgi.py
+++ ThreatIntel/ThreatIntel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SerloBox project.
+WSGI config for ThreatIntel project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -13,16 +13,12 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+import gevent.monkey
+gevent.monkey.patch_all()
+import pymysql
+pymysql.install_as_MySQLdb()
+import django.core.wsgi
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SerloBox.settings")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ThreatIntel.settings")
+application = django.core.wsgi.get_wsgi_application()
