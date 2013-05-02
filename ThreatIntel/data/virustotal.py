ef413ff5f7c5f14c5bb98974a760d462b515922f
Bharat Ponnaluri <bponnaluri@gmail.com>
2013-05-01 23:06:40 -0500

    Configured Virus Total API scans to use gevent.

--- ThreatIntel/data/virustotal.py
+++ ThreatIntel/data/virustotal.py
@@ -7,17 +7,20 @@ import json
 import urllib
 import urllib2
 import pprint
+import requests
 from .base import *
 
 class Query:
     def __init__(self,qLoc):
         self.queryLoc=qLoc
         self.resultJSON=dict()
-        self.result=DISP_FAILURE
     def setThreat(self,res):
-        self.result=res
+        self.resultJSON['response_code']=res
     def getThreatCode(self):
-        return self.result
+        if 'resonse_code' in self.resultJSON:
+            return self.resultJSON['response_code']
+        else:
+            return DISP_INDETERMINATE
     def getQueryLoc(self):
         return self.queryLoc
     def setResults(self,data):
@@ -45,9 +48,6 @@ class Query:
 
 #Convert tabs to spaces.
 #Make code more readable and look good.
-#Look at PhishTank code to fix syntax.
-#Python requests.
-#Make key consistent with the other APIs.
 class VirusTotalDataProvider(DataProvider):
     URL_SLOC="https://www.virustotal.com/vtapi/v2/url/report" #Location of URL scan API.
     IP_SLOC="https://www.virustotal.com/vtapi/v2/ip-address/report" #Location of IP scan API.
@@ -60,12 +60,8 @@ class VirusTotalDataProvider(DataProvider):
     def scanURL(self,query): 
         '''This method scans a URL and prints the number of positive scans'''
         parameters={"resource":query.getQueryLoc(), "apikey":self._apikey}
-        data=urllib.urlencode(parameters)
-        req=urllib2.Request(self.URL_SLOC,data)
-        response=urllib2.urlopen(req)
-        jsonStr=response.read()
-        jStr=json.loads(jsonStr)
-        
+        r=requests.post(self.URL_SLOC,parameters) 
+        jStr=r.json()     
         if jStr['positives']>2:
             query.setThreat(DISP_POSITIVE)
         elif jStr['positives']==0:
@@ -82,52 +78,44 @@ class VirusTotalDataProvider(DataProvider):
         '''This method scans an IP address
            There is currently a socket error here'''
         parameters = {'ip':'173.194.46.67', 'apikey':self._apikey}
-        response = urllib.urlopen('%s?%s' % (self.IP_SLOC, urllib.urlencode(parameters))).read()
-        response_dict = json.loads(response)
-
-        if('resolutions' in response_dict)==False: #We are querying an invalid IP address.
-            query.setThreat(DISP_INDETERMINATE)
-            query.setResults(response_dict)
-            return
-
-        pp=pprint.PrettyPrinter(indent=4)
-        pp.pprint(response_dict)
-        
-        if  ('detected_urls' in response_dict)==False:#Sometimes, data about the scan is not sent.
-            query.setThreat(DISP_POSITIVE)
-            query.setResults(response_dict)
-            return
-        sumData=response_dict['detected_urls']
-        if sumData['positives']>2: 
-            query.setThreat(DISP_POSITIVE)
-        elif sumData['positives']==0:
-            query.setThreat(DISP_NEGATIVE)
-        else:
-            query.setThreat(DISP_INDETERMINATE) 
-            query.setResults(response_dict)
+        r=requests.post(self.URL_SLOC,parameters)
+        try:
+            response_dict =r.json()
+            if('resolutions' in response_dict)==False: #We are querying an invalid IP address.
+                query.setThreat(DISP_INDETERMINATE)
+                query.setResults(response_dict)
+                return        
+            if  ('detected_urls' in response_dict)==False:#Sometimes, data about the scan is not sent.
+                query.setThreat(DISP_POSITIVE)
+                query.setResults(response_dict)
+                return
+            sumData=response_dict['detected_urls']
+            if sumData['positives']>2: 
+                query.setThreat(DISP_POSITIVE)
+            elif sumData['positives']==0:
+                query.setThreat(DISP_NEGATIVE)
+            else:
+                query.setThreat(DISP_INDETERMINATE) 
+                query.setResults(response_dict)
+        except ValueError:
+            query.setThreat(DISP_FAILURE) 
 
     def retrieveReport(query):
         '''This method scans a domain and returns results for that domain'''
         url = 'https://www.virustotal.com/vtapi/v2/domain/report'
         parameters = {'domain': '027.ru', 'apikey':self._apikey}
-        response = urllib.urlopen('%s?%s' % (url, urllib.urlencode(parameters))).read()
-        response_dict = json.loads(response)
+        r=requests.post(self.URL_SLOC,parameters)
+        response_dict =r.json()
         query.setResults(response_dict)
-
-
         
     def _query(self,target,qtype):
-       aDict={QUERY_URL:self.scanURL,QUERY_IPV4:self.scanIP,QUERY_DOMAIN:self.retrieveReport}    
-      
+
+       aDict={QUERY_URL:self.scanURL,QUERY_IPV4:self.scanIP,QUERY_DOMAIN:self.retrieveReport}      
        while True:
-           #qLoc=raw_input("Enter location to query")
            qLoc=target
            query=Query(qLoc)
-            
            if (qtype in aDict):
                aDict[qtype](query)
-  
-
            results=InformationSet(query.getThreatCode(),**query.getResultSummary())
            return results
       
