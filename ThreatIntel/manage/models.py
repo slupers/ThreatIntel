d422eb4921084c5dabea7df1235c9415c9ba9810
Andrew Mintner <mintner1@illinois.edu>
2013-05-02 18:54:58 -0500

    Simplified API key view/template using ModelForm; restructured apikey model; converted tabs to spaces

--- ThreatIntel/manage/models.py
+++ ThreatIntel/manage/models.py
@@ -1,37 +1,32 @@
 from django.forms import ModelForm
 from django.db import models
+from django.contrib.auth.models import User
 
 class apikeys(models.Model):
-	user = models.CharField(max_length=255, primary_key=True)
-	titankey = models.CharField(max_length=512)
-	icskey = models.CharField(max_length=512)
-	dshieldkey = models.CharField(max_length=512)
-	cifkey = models.CharField(max_length=512)
-	vtotkey = models.CharField(max_length=512)
-	ptankkey = models.CharField(max_length=512)
-	sserverkey = models.CharField(max_length=512)
+    user = models.OneToOneField(User, primary_key=True)
+    titancert = models.TextField(blank=True)
+    titankey = models.TextField(blank=True)
+    cifkey = models.CharField(max_length=36, blank=True)
+    vtotkey = models.CharField(max_length=64, blank=True)
+    ptankkey = models.CharField(max_length=512, blank=True) # FIXME
 
 class KeysForm(ModelForm):
-	class Meta:
-		model = apikeys
-		fields = ('titankey', 'icskey', 'dshieldkey', 'cifkey', 'vtotkey', 'ptankkey', 'sserverkey' )
-	def __init__(self, *args, **kwargs):
-		super(KeysForm, self).__init__(*args, **kwargs)
-		self.fields['titankey'].required = False
-		self.fields['icskey'].required = False
-		self.fields['dshieldkey'].required = False
-		self.fields['cifkey'].required = False
-		self.fields['vtotkey'].required = False
-		self.fields['ptankkey'].required = False
-		self.fields['sserverkey'].required = False
+    class Meta:
+        model = apikeys
+        fields = ('titancert', 'titankey', 'cifkey', 'vtotkey', 'ptankkey')
+    def __init__(self, *args, **kwargs):
+        super(KeysForm, self).__init__(*args, **kwargs)
+        self.fields['titancert'].required = False
+        self.fields['titankey'].required = False
+        self.fields['cifkey'].required = False
+        self.fields['vtotkey'].required = False
+        self.fields['ptankkey'].required = False
 
 class qry(models.Model):
-	query = models.CharField(max_length=512)
-	#query_type = models.CharField(max_length=255, choices=[('ipv4','IPv4'),('ipv6','IPv6'),('fqdn','FQDN'),('url','URL'),('hash','Malware hash')])
+    query = models.CharField(max_length=512)
+    #query_type = models.CharField(max_length=255, choices=[('ipv4','IPv4'),('ipv6','IPv6'),('fqdn','FQDN'),('url','URL'),('hash','Malware hash')])
 
 class QueryForm(ModelForm):
-	class Meta:
-		model = qry
-		fields = ('query',)#, 'query_type')
-
-
+    class Meta:
+        model = qry
+        fields = ('query',)#, 'query_type')
--- ThreatIntel/manage/views.py
+++ ThreatIntel/manage/views.py
@@ -34,7 +34,7 @@ def handle_query(user, query):
     api_info = []
 
     # get api keys of the user
-    account = apikeys.objects.get(user=user)
+    account = apikeys.objects.get(user__username=user)
 
     # intialize providers
     dshield = DShieldDataProvider()
@@ -64,32 +64,19 @@ def handle_query(user, query):
 @login_required(redirect_field_name='/login')
 def get_keys(request):
     '''Processes and displays keys API keys entered by user'''
-    user = request.user.username
+    user = request.user
     state = 'Please enter your API keys'
     # if user submitted the form
-    if request.method == 'POST':
-        # put user-entered information in form
-        form = KeysForm(request.POST)
-
-        # if form is ok, save the file and return user to this page
-        if form.is_valid():
-            keys = apikeys(user=user)
-            keys.titankey = request.POST.get('titankey')
-            keys.icskey = request.POST.get('icskey')
-            keys.dshieldkey = request.POST.get('dshieldkey')
-            keys.cifkey = request.POST.get('cifkey')
-            keys.vtotkey = request.POST.get('vtotkey')
-            keys.ptankkey = request.POST.get('ptankkey')
-            keys.sserverkey = request.POST.get('sserverkey')
-            keys.save()
-            state = 'Your keys have been saved!'
-
-    form = KeysForm()
     try:
-        account = apikeys.objects.get(user=user)
-        # load the page with the form and any data already supplied by user
-        return render_to_response('apikeys.html', {'form': form, 'state': state, 'titankey': account.titankey, 'icskey': account.icskey, 'dshieldkey': account.dshieldkey, 'cifkey': account.cifkey, 'vtotkey': account.vtotkey, 'ptankkey': account.ptankkey, 'sserverkey': account.sserverkey}, RequestContext(request))
+        inst = apikeys.objects.get(user=user)
     except apikeys.DoesNotExist:
-        # load the page with the empty form
-        return render_to_response('apikeys.html', {'form': form, 'state': state}, RequestContext(request))
-
+        inst = None
+    print(inst)
+    if request.method == 'POST':
+        form = KeysForm(request.POST, instance=inst)
+        if form.is_valid():
+            form.save()
+            state = 'Your keys have been saved!'
+    else:
+        form = KeysForm(instance=inst)
+    return render_to_response('apikeys.html', {'form': form, 'state': state}, RequestContext(request))
