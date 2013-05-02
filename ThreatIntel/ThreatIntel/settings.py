75a072dba1c7c4f4013b0e128e8c3df573224f68
Andrew Mintner <mintner1@illinois.edu>
2013-05-02 17:22:13 -0500

    Cleanup of various Django files

--- ThreatIntel/ThreatIntel/settings.py
+++ ThreatIntel/ThreatIntel/settings.py
@@ -1,177 +1,94 @@
-# Django settings for ThreatIntel project.
+#
+# Utility values
+#
+
+# Courtesy of http://www.morethanseven.net/2009/02/11/django-settings-tip-setting-relative-paths/
+import os.path
+SITE_BASE = os.path.dirname(os.path.realpath(__file__))
+
+#
+# Core settings
+#
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+ALLOWED_HOSTS = [] # FIXME
+TIME_ZONE = "Etc/UTC"
+LANGUAGE_CODE = "en-us"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+WSGI_APPLICATION = "ThreatIntel.wsgi.application"
+ROOT_URLCONF = "ThreatIntel.urls"
+SECRET_KEY = "z$a6sqedr$ni2zil+7oktg9_4e@!znm_4pufmm9bw=3n#@&amp;fo@" # FIXME
+LOGGING_CONFIG = None
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+#
+# Backends
+#
+
+TEMPLATE_LOADERS = (
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+#     "django.template.loaders.eggs.Loader",
 )
-
-MANAGERS = ADMINS
-
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware", 
+    #"django.middleware.csrf.CsrfResponseMiddleware",
+    # Uncomment the next line for simple clickjacking protection:
+    # "django.middleware.clickjacking.XFrameOptionsMiddleware",
+)
+TEMPLATE_DIRS = (
+    os.path.join(SITE_BASE, "../templates")
+)
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    #"django.contrib.sites",
+    "django.contrib.messages",
+    #"django.contrib.staticfiles",
+    "manage",
+    # Uncomment the next line to enable the admin:
+    # "django.contrib.admin",
+    # Uncomment the next line to enable admin documentation:
+    # "django.contrib.admindocs",
+)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'luperso1_threat',
-        'HOST': 'engr-cpanel-mysql.engr.illinois.edu',
-        'USER': 'luperso1_master',
-        'PASSWORD': 'sec460lab'
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "luperso1_threat",
+        "HOST": "engr-cpanel-mysql.engr.illinois.edu",
+        "USER": "luperso1_master",
+        "PASSWORD": "sec460lab"
     }
 }
 
-CPANEL_DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangostack',
-        'HOST': '/opt/djangostack-1.4.5-0/mysql/tmp/mysql.sock',
-        'PORT': '33060',
-        'USER': 'bitnami',
-        'PASSWORD': 'ad1a131288'
-    }
-}
+#
+# Static files
+#
+
+STATIC_ROOT = ""
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+	os.path.join(SITE_BASE, "../static")
+]
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#    "django.contrib.staticfiles.finders.DefaultStorageFinder",
+]
+
+#
+# Authentication
+#
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
 )
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en//ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/home/slupers/GitWorkspace/ThreatIntel/ThreatIntel/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-	'/home/slupers/GitWorkspace/ThreatIntel/ThreatIntel/static',
-)
-
-LOGIN_URL = '/login/'
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'z$a6sqedr$ni2zil+7oktg9_4e@!znm_4pufmm9bw=3n#@&amp;fo@'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware', 
-    #'django.middleware.csrf.CsrfResponseMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'ThreatIntel.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'ThreatIntel.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-	'/home/slupers/GitWorkspace/ThreatIntel/ThreatIntel/templates',
-    #'/home/carlo/djangostack-1.4.5-0/apps/django/django_projects/SerloBox/templates'
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'manage',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-)
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+LOGIN_URL = "/login/"
--- ThreatIntel/manage.py
+++ ThreatIntel/manage.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python
-#import gevent.monkey
-#gevent.monkey.patch_all()
-import sys
-sys.path = sys.path + ['', '/usr/lib/python2.7', '/usr/lib/python2.7/plat-linux2', '/usr/lib/python2.7/lib-tk', '/usr/lib/python2.7/lib-old', '/usr/lib/python2.7/lib-dynload', '/usr/local/lib/python2.7/dist-packages', '/usr/lib/python2.7/dist-packages', '/usr/lib/python2.7/dist-packages/PIL', '/usr/lib/python2.7/dist-packages/gst-0.10', '/usr/lib/python2.7/dist-packages/gtk-2.0', '/usr/lib/pymodules/python2.7', '/usr/lib/python2.7/dist-packages/ubuntu-sso-client', '/usr/lib/python2.7/dist-packages/ubuntuone-client', '/usr/lib/python2.7/dist-packages/ubuntuone-control-panel', '/usr/lib/python2.7/dist-packages/ubuntuone-couch', '/usr/lib/python2.7/dist-packages/ubuntuone-installer', '/usr/lib/python2.7/dist-packages/ubuntuone-storage-protocol'] 
-
+#!/usr/bin/python
+import gevent.monkey
+gevent.monkey.patch_all()
+import django.core.management
 import os
 import sys
 
-
-if __name__ == "__main__":
-    import gevent
-    from gevent import monkey
-    monkey.patch_all()
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ThreatIntel.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+if __name__ == b"__main__":
+    os.environ.setdefault(b"DJANGO_SETTINGS_MODULE", b"ThreatIntel.settings")
+    django.core.management.execute_from_command_line(sys.argv)
