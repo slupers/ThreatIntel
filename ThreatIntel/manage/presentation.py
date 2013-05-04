d088a6103840339f34f494de28c2ef3c8ac8b3c4
Andrew Mintner <mintner1@illinois.edu>
2013-05-04 08:59:20 -0500

    Enhanced presentation code

--- ThreatIntel/manage/presentation.py
+++ ThreatIntel/manage/presentation.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import abc
 import binascii
+import datetime
+import django.utils.formats
 import django.utils.html
+
+#
+# Presentable structures
+#
 
 class AttributeList(list):
     def __init__(self):
@@ -11,12 +18,17 @@ class AttributeList(list):
         tag = x[0]
         value = x[1]
         assert isinstance(tag, unicode)
-        assert type(value) in _presenters
+        assert isinstance(value, Presentable)
         super(AttributeList, self).append((tag, value))
     
-    @classmethod
-    def _present(cls, value):
-        pass
+    def as_table(self):
+        val = "<table>"
+        for k, v in self:
+            c1 = django.utils.html.escape(k) # TODO: translate me
+            c2 = django.utils.html.escape(v)
+            val += "<tr><th>{0}</th><td>{1}</td></tr>".format(c1, c2)
+        val += "</table>"
+        return val
 
 class EntityList(list):
     def __init__(self, column_tags):
@@ -27,34 +39,56 @@ class EntityList(list):
     
     def append(self, x):
         assert len(x) == len(self._ctags)
-        assert all((type(e) in _presenters for e in x))
+        assert all((isinstance(e, Presentable) for e in x))
         super(EntityList, self).append(tuple(x))
         
     @property
     def columns(self):
         return self._ctags
-    
-    @classmethod
-    def _present(cls, value):
-        pass
+        
+    def as_table(self):
+        val = "<table><thead>"
+        for v in self._ctags:
+            c = django.utils.html.escape(v) # TODO: translate me
+            val += "<th>{0}</th>".format(c)
+        val += "</thead><tbody>"
+        for v in self:
+            val += "<tr>"
+            for e in v:
+                c = django.utils.html.escape(e)
+                val += "<td>{0}</td>".format(c)
+            val += "</tr>"
+        val += "</tbody></table>"
+        return val
+
+#
+# Object presentation
+#
 
 def _present_bytes(value):
     return binascii.hexlify(value).decode()
 
-def _present_date(value):
-    pass
-
-def _present_datetime(value):
-    pass
+def _present_generic(value):
+    return django.utils.html.escape(django.utils.formats.localize(value))
 
 def _present_unicode(value):
     return django.utils.html.escape(value)
 
 _presenters = {
-    AttributeList: AttributeList._present,
+    AttributeList: AttributeList.as_table,
+    bool: _present_generic,
     bytes: _present_bytes,
-    date: _present_date,
-    datetime: _present_datetime,
-    EntityList: EntityList._present,
+    datetime.date: _present_generic,
+    datetime.datetime: _present_generic,
+    EntityList: EntityList.as_table,
+    float: _present_generic,
+    int: _present_generic,
+    long: _present_generic,
     unicode: _present_unicode
 }
+
+class Presentable(object):
+    __metaclass__ = abc.ABCMeta
+
+for t in _presenters.keys():
+    Presentable.register(t)
