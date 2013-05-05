5f00d406139258f6d24d93d40d5a94f593e74373
Andrew Mintner <mintner1@illinois.edu>
2013-05-04 19:18:36 -0500

    Refactored DShieldDataProvider

--- ThreatIntel/data/dshield.py
+++ ThreatIntel/data/dshield.py
@@ -8,59 +8,30 @@ from xml.etree.cElementTree import XMLParser
 from .base import *
 from manage.presentation import *
 
+def handle_date(value):
+    if value == "0":
+        return None
+    return datetime.strptime(value, "%Y-%m-%d").date()
+
+def handle_datetime(value):
+    if value == "0":
+        return None
+    return datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+
 class DShieldDataProvider(DataProvider):
-    _endpoint = "http://www.dshield.org/api/ip/{0}"
-    
     @classmethod
     def _parse(cls, data):
         # Produce an AttributeList from the data
         info = AttributeList()
-        attacks = None
-        ev = data.get("mindate")
-        if ev != None:
-            if ev == "0":
-                return None
-            dv = datetime.strptime(ev, "%Y-%m-%d").date()
-            info.append(("first_event_ts", dv))
-        ev = data.get("maxdate")
-        if ev != None:
-            if ev == "0":
-                return None
-            dv = datetime.strptime(ev, "%Y-%m-%d").date()
-            info.append(("last_event_ts", dv))
-        ev = data.get("updated")
-        if ev != None:
-            if ev == "0":
-                return None
-            dtv = datetime.strptime(ev, "%Y-%m-%d %H:%M:%S")
-            info.append(("update_ts", dtv))
-        ev = data.get("count")
-        if ev != None:
-            info.append(("n_attack_packets", int(ev)))
-        ev = data.get("attacks")
-        if ev != None:
-            attacks = int(ev)
-            info.append(("n_attack_targets", attacks))
-        ev = data.get("country")
-        if ev != None:
-            info.append(("country", ev))
-        ev = data.get("as")
-        if ev != None:
-            info.append(("as_number", int(ev)))
-        ev = data.get("asname")
-        if ev != None:
-            info.append(("as_name", ev))
-        ev = data.get("network")
-        if ev != None:
-            info.append(("network_prefix", ev))
-        ev = data.get("comment")
-        if ev != None:
-            info.append(("comment", ev))
-        ev = data.get("abusecontact")
-        if ev != None:
-            info.append(("abuse_contact", ev))
+        for key, newkey, fn in cls._handlers:
+            value = data.get(key)
+            if value != None:
+                newvalue = fn(value)
+                if newvalue != None:
+                    info.append((newkey, newvalue))
         
         # Determine a disposition and return the InformationSet
+        attacks = int(data["attacks"]) if "attacks" in data else None
         if attacks == None:
             disp = DISP_INFORMATIONAL
         elif attacks < 10:
@@ -93,3 +64,22 @@ class DShieldDataProvider(DataProvider):
             if len(value) != 0:
                 data[tag] = value
         return self._parse(data)
+    
+    _endpoint = "http://www.dshield.org/api/ip/{0}"
+    _handlers = [
+        ("mindate", "first_event_ts", handle_date),
+        ("maxdate", "last_event_ts", handle_date),
+        ("updated", "update_ts", handle_datetime),
+        ("count", "n_attack_packets", int),
+        ("attacks", "n_attack_targets", int),
+        ("country", "country", lambda x: x),
+        ("as", "as_number", int),
+        ("asname", "as_name", lambda x: x),
+        ("network", "network_prefix", lambda x: x),
+        ("comment", "comment", lambda x: x),
+        ("abusecontact", "abuse_contact", lambda x: x)
+    ]
+
+__all__ = [
+    b"DShieldDataProvider"
+]
